bb8c129a41f048e8724721839b7399f664803085
vladblip <ezio2506@gmail.com>
2023-06-02 11:20:55 +0300

    Created cart page

---
+++ app/cart/page.js
@@ -0,0 +1,52 @@
+"use client";
+
+import { useRef } from "react";
+import Form from "../components/form/Form";
+import Cart from "../components/cart/Cart";
+
+export default function CartPage() {
+  const formRef = useRef();
+
+  const onSubmitHandler = (cart) => {
+    const form = formRef.current;
+
+    const credentials = {
+      name: form.name.value,
+      email: form.email.value,
+      phone: form.phone.value,
+      address: form.address.value,
+    };
+
+    async function sendOrder() {
+      try {
+        const response = await fetch("http://localhost:5000/order", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            credentials,
+            cart,
+          }),
+        });
+        const data = await response.text();
+
+        alert(data);
+      } catch (err) {
+        alert(err);
+      }
+    }
+
+    sendOrder();
+  };
+  return (
+    <main className="grid grid-cols-2 gap-10 h-full">
+      <div className="bg-blue-100 rounded-xl grid place-content-center">
+        <Form ref={formRef} />
+      </div>
+      <div className="bg-blue-100 rounded-xl p-20">
+        <Cart onSubmit={onSubmitHandler} />
+      </div>
+    </main>
+  );
+}
