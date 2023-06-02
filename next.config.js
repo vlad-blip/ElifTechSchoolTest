035bdab640cc1e0d0ab1fddf0041e31532e4aa3f
vladblip <ezio2506@gmail.com>
2023-06-02 11:19:22 +0300

    Added pictures route to config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "papa-joe.com.ua",
+        port: "",
+        pathname: "/wp-content/uploads/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
