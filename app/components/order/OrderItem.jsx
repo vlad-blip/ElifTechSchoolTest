48e473578e57f97e6b770b8a52a6d2e57b443de0
vladblip <ezio2506@gmail.com>
2023-06-02 21:12:21 +0300

    Created order item component

---
+++ app/components/order/OrderItem.jsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+
+export default function OrderItem({
+  title,
+  image,
+  price,
+  totalPrice,
+  quantity,
+}) {
+  return (
+    <div className="bg-white rounded-xl overflow-hidden flex items-center gap-10">
+      <Image
+        className=""
+        src={image}
+        width={200}
+        height={100}
+        alt={`${title} image`}
+      />
+      <div>
+        <h3 className="text-4xl mb-2">{title}</h3>
+        <p className="text-xl">
+          Price: {price}x{quantity} Total Price: {totalPrice}
+        </p>
+      </div>
+    </div>
+  );
+}
