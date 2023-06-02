f5ba884f39a0c6eb473205f0fe53714dd78398e4
vladblip <ezio2506@gmail.com>
2023-06-02 21:10:13 +0300

    Added coupons

--- app/components/cart/Cart.jsx
+++ app/components/cart/Cart.jsx
@@ -1,18 +1,41 @@
 "use client";
 
-import { useSelector } from "react-redux";
+import { useRef } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { addCoupon } from "@/store/cartSlice";
 import CartList from "./CartList";
 
 export default function Cart({ onSubmit }) {
+  const couponRef = useRef("");
+
   const cart = useSelector((state) => state.cart);
 
+  const dispatch = useDispatch();
+
+  const addCouponHandler = () => {
+    dispatch(addCoupon(couponRef.current.value));
+  };
   return (
-    <div className="flex flex-col justify-between">
+    <div className="flex flex-col justify-between gap-4">
       <CartList />
       <div className="flex items-center gap-10">
         <p className="text-3xl">
           Total price: <span className="font-bold">{cart.totalPrice}</span>
         </p>
+        <div className="rounded-md overflow-hidden">
+          <input
+            ref={couponRef}
+            className="py-5 px-2"
+            type="text"
+            placeholder="Coupon code"
+          />
+          <button
+            className="bg-black text-white py-5 px-2"
+            onClick={addCouponHandler}
+          >
+            Apply
+          </button>
+        </div>
         <button
           className="bg-white text-2xl py-4 px-8 rounded-md"
           onClick={onSubmit.bind(null, cart)}
