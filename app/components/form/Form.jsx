dd7531507fc9a6cb003f8ab4c636c5745f62c5a1
vladblip <ezio2506@gmail.com>
2023-06-02 11:22:32 +0300

    Created user form component

---
+++ app/components/form/Form.jsx
@@ -0,0 +1,47 @@
+import { forwardRef } from "react";
+
+export default forwardRef(function Form(_, ref) {
+  return (
+    <form
+      ref={ref}
+      className="flex flex-col gap-5 w-fit h-fit border-2 border-black rounded-md p-5"
+    >
+      <label className="flex gap-2 justify-between items-center">
+        Name:
+        <input
+          name="name"
+          className="p-2 rounded-md"
+          type="text"
+          placeholder="Write your name"
+        />
+      </label>
+      <label className="flex gap-2 justify-between items-center">
+        Email:
+        <input
+          name="email"
+          className="p-2 rounded-md"
+          type="email"
+          placeholder="Write your email"
+        />
+      </label>
+      <label className="flex gap-2 justify-between items-center">
+        Phone:
+        <input
+          name="phone"
+          className="p-2 rounded-md"
+          type="tel"
+          placeholder="Write your phone"
+        />
+      </label>
+      <label className="flex gap-2 justify-between items-center">
+        Address:
+        <input
+          name="address"
+          className="p-2 rounded-md"
+          type="text"
+          placeholder="Write your address"
+        />
+      </label>
+    </form>
+  );
+});
