865d232087cabc073617e0dd17bf3f6b2fad86f6
vladblip <ezio2506@gmail.com>
2023-06-03 12:52:21 +0300

    Added redux persist

--- store/index.js
+++ store/index.js
@@ -1,13 +1,47 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import thunk from "redux-thunk";
+
+import storage from "redux-persist/lib/storage";
 
 import productSlice from "./productSlice";
 import cartSlice from "./cartSlice";
 
-const store = configureStore({
-  reducer: {
-    product: productSlice,
-    cart: cartSlice,
-  },
+import historySlice from "./historySlice";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"],
+};
+
+const rootReducer = combineReducers({
+  product: productSlice,
+  cart: cartSlice,
+  history: historySlice,
 });
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PURGE, REGISTER, PERSIST],
+      },
+    }),
+  thunk,
+});
+
+export const persistor = persistStore(store);
