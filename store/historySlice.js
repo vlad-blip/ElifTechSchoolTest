a7a32e8c5d8f2c7f9fdfef5bfee590260978b948
vladblip <ezio2506@gmail.com>
2023-06-03 12:52:33 +0300

    Improved error handling

--- store/historySlice.js
+++ store/historySlice.js
@@ -3,15 +3,25 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 export const fetchHistory = createAsyncThunk(
   "history/fetchHistory",
   async ({ email, phone }) => {
-    const response = await fetch("http://localhost:5000/history", {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify({ email, phone }),
-    });
-    const data = await response.json();
-    return data;
+    try {
+      const response = await fetch("http://localhost:5000/history", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({ email, phone }),
+      });
+
+      if (response.status === 200) {
+        const data = await response.json();
+        return data;
+      } else {
+        const error = await response.text();
+        throw new Error(error);
+      }
+    } catch (error) {
+      throw new Error(error);
+    }
   }
 );
 
@@ -26,6 +36,7 @@ const historySlice = createSlice({
   extraReducers: (builder) => {
     builder.addCase(fetchHistory.rejected, (state, action) => {
       state.status = "rejected";
+      alert(action.error.message);
     });
     builder.addCase(fetchHistory.fulfilled, (state, action) => {
       return {
