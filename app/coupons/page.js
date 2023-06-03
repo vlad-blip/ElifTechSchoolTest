1952a67a0a77ac868c09c3fa7d417f446a485e86
vladblip <ezio2506@gmail.com>
2023-06-03 12:54:00 +0300

    Coupon page

---
+++ app/coupons/page.js
@@ -0,0 +1,9 @@
+import CouponsList from "../components/coupons/CouponsList";
+
+export default function Coupons() {
+  return (
+    <main className="bg-blue-100 h-full p-10 rounded-xl">
+      <CouponsList />
+    </main>
+  );
+}
